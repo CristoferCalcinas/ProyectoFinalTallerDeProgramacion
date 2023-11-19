beb6247fef501bba1eaef6b2eff23728840f8f70
carlosraulcala <calacamatacarlosraul@gmail.com>
2023-11-18 21:38:13 -0400

    Arreglando conflicto de versiones

--- Frontend/src/Components/moleculas/InputLoginForm.js
+++ Frontend/src/Components/moleculas/InputLoginForm.js
@@ -1,39 +1,81 @@
 import { Box, Button, Typography } from '@mui/material'
 import TextInput from '../atomos/TextInput'
+import { useForm } from 'react-hook-form'
 
 export default function InputLoginForm() {
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const onSubmit = (data) => {
+        console.log("Form data:", data);
+        reset();
+    };
+
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                padding: '20px',
-                borderRadius: '20px',
-                // bgcolor: 'red',
-                gap: '20px',
-            }}
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    padding: '20px',
+                    borderRadius: '20px',
+                    // bgcolor: 'red',
+                    gap: '20px',
+                }}
 
-        >
-            <Typography variant="h4" style={{ color: 'white' }}>
-                Sign In
-            </Typography>
-
-
-            <TextInput label={'Email'} margin="none" width={'400px'} />
-            <TextInput label={'Password'} margin="normal" width={'400px'} />
-
-            <Button
-                variant="contained"
-                color="secondary"
-                style={{ width: '400px', marginTop: '12px', backgroundColor: '#000', color: '#fff', borderRadius: '10px', height: '50px' }}
             >
-                SIGN IN
-            </Button>
+                <Typography variant="h4" style={{ color: 'white' }}>
+                    Sign In
+                </Typography>
 
-            <Typography variant="h4" style={{ color: 'white' }}>
-                Sign Up
-            </Typography>
-        </Box>
+
+                <TextInput name={'Email'} margin="none" width={'400px'}
+                    errors={errors}
+                    registerparam={
+                        {
+                            ...register("email", {
+                                required: "Email is required",
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+                                    message: "Correo no valido - ejemplo: correo@google.com",
+                                },
+                            }),
+                        }
+                    }
+                />
+                <TextInput name={'Password'} margin="normal" width={'400px'}
+                    errors={errors}
+                    registerparam={
+                        {
+                            ...register("password", {
+                                required: "Password is required",
+                                minLength: {
+                                    value: 8,
+                                    message: "Password must be at least 8 characters",
+                                },
+                                maxLength: {
+                                    value: 20,
+                                    message: "Password must be less than 20 characters",
+                                },
+                            }),
+                        }
+                    }
+                />
+
+                <Button
+                    type='submit'
+                    variant="contained"
+                    color="secondary"
+                    style={{ width: '400px', marginTop: '12px', backgroundColor: '#000', color: '#fff', borderRadius: '10px', height: '50px' }}
+                >
+                    SIGN IN
+                </Button>
+
+                <Typography variant="h4" style={{ color: 'white' }}>
+                    Sign Up
+                </Typography>
+            </Box>
+        </form>
     )
 }
--- Frontend/src/Components/pages/LoginPage.js
+++ Frontend/src/Components/pages/LoginPage.js
@@ -1,7 +1,7 @@
 import NavBar from "../organismos/NavBar";
 import LoginForm from "../organismos/LoginForm";
 
-export default function Page() {
+export default function LoginPage() {
     return (
         <>
             <div>
