09925288ea8c0a229ff7de6d2f452137fe900685
jimmy <jimmy@gmail.com>
2023-11-23 09:06:39 -0400

    he creado controller, modelo de nombre anuncios

---
+++ Backend/BE-proyectoFinal/Contexto/AplicacionContexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Modelo;
+
+namespace WebApplication3.Contexto
+{
+    public class AplicacionContexto : DbContext
+    {
+        public AplicacionContexto(DbContextOptions<AplicacionContexto> options):base(options) { }
+        public DbSet<Universidad> universidad { get; set; }
+        public DbSet<Estudiante> estudiante { get; set; }
+        public DbSet<Docente> docente { get; set; }
+        public DbSet<Materias> materias { get; set; }
+    }
+}
