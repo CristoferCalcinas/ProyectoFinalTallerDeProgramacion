8bf20d48b8a3260c4926046721f95329c69a8e98
adrian <adrian@google.com>
2023-11-24 11:01:31 -0400

    ordenando los archivos

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,25 @@
-//import RegisterPage from "./Components/pages/RegisterPage";
-//import ModelCarsPage from "./Components/pages/ModelCarsPage";
-//import ListCars from "./Components/pages/ListCars";
-//import PostForm from "./Components/pages/PostForm";
+import ListCars from "./Components/pages/ListCars";
+import LoginPage from "./Components/pages/LoginPage";
+import RegisterPage from "./Components/pages/RegisterPage";
+import AdCars from "./Components/pages/AdCars";
+import ProfilePage from "./Components/pages/ProfilePage";
+import PostForm from "./Components/pages/PostForm";
+import CarsbyMakePage from "./Components/pages/FindCarsAdvancedPage";
+import ModelCarsPage from "./Components/pages/ModelCarsPage";
 import FindCarsAdvancedPage from "./Components/pages/FindCarsAdvancedPage";
 
 function App() {
   return (
     <div className="App">
       {/* Pagina de Formulario para publicar*/}
+      <ListCars />
+      <LoginPage />
+      <RegisterPage />
+      <AdCars />
+      <ProfilePage />
+      <PostForm />
+      <CarsbyMakePage />
+      <ModelCarsPage />
       <FindCarsAdvancedPage />
     </div>
   );
